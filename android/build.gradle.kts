46ebe03358b99d52bf976f982c6f5d05e6616dc2
htoopyaelinn56 <hplinn0@gmail.com>
2026-06-13 18:53:06 +0630

    feat: configure NDK version for subprojects in build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,23 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    val configureNdk: Project.() -> Unit = {
+        if (extensions.findByName("android") != null) {
+            configure<com.android.build.gradle.BaseExtension> {
+                ndkVersion = "30.0.14904198"
+            }
+        }
+    }
+    if (state.executed) {
+        configureNdk()
+    } else {
+        afterEvaluate {
+            configureNdk()
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
